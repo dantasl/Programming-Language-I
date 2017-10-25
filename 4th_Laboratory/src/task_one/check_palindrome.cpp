66fc491336b100337a8c266dbae5b84fbdc447ec
Lucas Gomes Dantas <dantaslucas@A205-08L.imd.local>
2017-10-25 14:00:21 -0300

    Removing blank spaces of strings with any ammount of spaces.

--- 4th_Laboratory/src/task_one/check_palindrome.cpp
+++ 4th_Laboratory/src/task_one/check_palindrome.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-	string a = "Meu Deus, BiáááÀno he";
+	string a = "Meu Deus, BiáááÀno              he";
 	remove_blank_space(a);
 	cout << a << endl;
 	transform(a.begin(), a.end(), a.begin(), ::tolower);//first you must replace accents
--- 4th_Laboratory/src/task_one/string_manipulation.cpp
+++ 4th_Laboratory/src/task_one/string_manipulation.cpp
@@ -1,10 +1,12 @@
 #include "task_one/string_manipulation.h"
 
-void remove_blank_space(string& s) // do not work for multiple spaces. fix it later.
+void remove_blank_space(string& s)
 {
-	for(auto i(0u); i < s.size(); ++i) //iterates over the string
+	for(auto i(0u); i < s.size();) //iterates over the string
 	{
 		if( isblank( s[i] ) ) //checks if the char is a blank space
 			s.erase(i, 1); //erase 1 char at position i
+		else
+			++i; //only increments i if string is not a blank space
 	}
 }
