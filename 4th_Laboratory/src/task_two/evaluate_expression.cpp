29fc7bc70948871383c1ac038c44d1920a0e8ee4
Lucas <lucas_gomesdantas@hotmail.com>
2017-10-27 13:39:11 -0300

    Changing dialog with user.

--- 4th_Laboratory/src/task_two/evaluate_expression.cpp
+++ 4th_Laboratory/src/task_two/evaluate_expression.cpp
@@ -2,9 +2,28 @@
 
 int main(int argc, char const *argv[])
 {
-	string s;
-	cout << "Type me something: " << endl;
-	getline(cin, s);
-	vector<string> postfix = split_sentence_blank_spaces(s);
-	compute_expression(postfix);
+	// Showing brief dialog with user and getting expression.
+	// ========================================================================================
+	string expression;
+	cout << endl << "===========================================================================" << endl;
+	cout << "My man! Welcome to my Postfix Calculator. Fancy name, eh?" << endl;
+	cout << endl << "Before you begin, you must know a few things:" << endl;
+	cout << "I - You *must* split each term of your equation with a single blank space." << endl;
+	cout << "II - You *must* provide a valid postfix expression." << endl;
+	cout << "Failing in any of these rules, the algorithm will be terminated." << endl;
+	cout << endl << "Now, please type in your expression: " << endl;
+	getline(cin, expression);
+	// ========================================================================================
+	
+	// Splitting expression's components by blank spaces.
+	// ========================================================================================
+	vector<string> postfix = split_sentence_blank_spaces(expression);
+	// ========================================================================================
+
+	// Informing the user of his expression's result.
+	// ========================================================================================
+	cout << endl <<  "And the result is: " << compute_expression(postfix) << endl;
+	// ========================================================================================
+
+	cout << "===========================================================================" << endl << endl;
 }
