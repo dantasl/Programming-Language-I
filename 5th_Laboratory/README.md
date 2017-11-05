cb3eaf2b0ad368b3c7cdfb6d8e8489991d4977b6
Lucas <lucas_gomesdantas@hotmail.com>
2017-11-05 19:01:39 -0300

    Adding task_four to project and writing its comments.

--- 5th_Laboratory/README.md
+++ 5th_Laboratory/README.md
@@ -9,7 +9,7 @@ This algorithm was developed for the Programming Language I class (<b>IMD/UFRN -
 * On command line, go to the directory where you've cloned this repository;
 * Type <code><b>make init</b></code>;
 * Type <code><b>make</b></code>;
-* If you want to execute question 1, type <code><b>./bin/check_palindrome</b></code>;
-* If you want to execute question 2, type <code><b>./bin/evaluate_expression</b></code>;
+* If you want to execute question 1, type <code><b>./bin/closest_to_mean</b></code>;
+* If you want to execute question 2, type <code><b>./bin/print_elements</b></code>;
 * If you want to generate Doxygen's documentation, type <code><b>make doxy</b></code>;
 * To clean project, type <code><b>make clean</b></code>.
--- 5th_Laboratory/src/task_four/task_four.cpp
+++ 5th_Laboratory/src/task_four/task_four.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file    task_four.cpp
+ * @author  Silvio da Costa Sampaio (codes)
+ * @author  Lucas Gomes Dantas (comments)
+ * @since   05/11/2017
+ * @date    05/11/2017
+ */
+
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -19,7 +27,7 @@ int square(int val) {
 
 int main(int argc, char* argv[]) {
 
-	// Creating vector col and col2. After, pushes back values [1, 9] to vector col.
+	// Creating vector col and col2. After, pushes back values [1, ..., 9] to vector col.
 	// ========================================================================================
 	vector<int> col;
 	vector<int> col2;
@@ -28,7 +36,12 @@ int main(int argc, char* argv[]) {
 	}
 	// ========================================================================================
 	
-	// std::transform = 
+	// std::back_inserter = Creates a back-insert iterator that inserts new elements at the end
+	// of the container to which it is applied.
+	// std::transform = Applies a given function to a range and stores the result in another.
+	// Here we will apply function square() to each element in range [ col.begin(), col.end() )
+	// and gonna store the result at the end (or "back") of container col2.
+	// After this, col2 will store the square of each element of col.
 	// ========================================================================================
 	transform(col.begin(), col.end(), back_inserter(col2), square);
 	// ========================================================================================
